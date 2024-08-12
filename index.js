99711026190bec7fcb4a10b3c498405891e3955c
sam-christopher07 <samchristophergit07@gmail.com>
2024-08-12 17:04:46 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = "Jonas";
+// let Job = "Software Engineer";
+// const year = 1991;
+
+// //Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+//Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted");
+}
+
